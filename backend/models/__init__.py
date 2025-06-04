0af7f9e47e664a49503daaa820f2683e8269cc17
huentemillaT <huentemillatania@gmail.com>
2025-06-03 22:40:07 -0400

    update

--- backend/models/__init__.py
+++
@@ -1,7 +0,0 @@
-#models/__init__.py
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from .user import User  # Importa aquí tus modelos para que estén disponibles al importar models
-
